563957c97e1986bbbef5ccb17a97f13a4429a29f
Diogenes <paolofersantella@gmail.com>
2026-02-25 23:08:17 -0400

    certifications update

---
+++ src/data/certifications.data.ts
@@ -0,0 +1,58 @@
+export interface Certifications {
+  image: string
+  title: string
+  duration: string
+}
+
+export const certifications: Certifications[] = [
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074179/aws_tt09dl.jpg",
+        title: "Aws for beginners",
+        duration: "4 hours"
+    },
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074179/clean-code_cybuqc.jpg",
+        title: "SOLID and clean code principles",
+        duration: "6.5 hours"
+    },
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074179/design-patterns_aa0mcn.jpg",
+        title: "Design patterns",
+        duration: "10 hours"
+    },
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074179/git_aw0yny.jpg",
+        title: "Git",
+        duration: "5.5 hours"
+    },
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074179/js_hwilzg.png",
+        title: "JavaScript",
+        duration: "52 hours"
+    },
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074179/nest_mr4hb0.jpg",
+        title: "NestJs A progressive Node.js framework",
+        duration: "24 hours"
+    },
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074180/node_ysi8iq.jpg",
+        title: "NodeJs",
+        duration: "37.5 hours"
+    },
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074180/postgresql_lnqewm.jpg",
+        title: "SQL - PostgreSQL",
+        duration: "16 hours"
+    },
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074180/react_ctl5av.png",
+        title: "React",
+        duration: "55 hours"
+    },
+    {
+        image: "https://res.cloudinary.com/dqclkzb8r/image/upload/v1772074180/shadcn_nec9sn.png",
+        title: "Shadcn styles library",
+        duration: "6 hours"
+    }
+]
