6a2c8c2e43183f9997cf02996f035140ab173efa
Diogenes Fermin <paolofersantella@gmail.com>
2026-08-23 21:15:59 -0400

    feat/fix-reload-git-grid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: netlify(),
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      /** @ts-ignore */
+      tailwindcss()
+    ]
   }
 });
